1879aba4de1e8f47ee406f4bbf5b4643e7d8e1ee
DanielFelipeDeveloper <danielfelipedeveloper@gmail.com>
2020-07-03 16:56:56 -0400

    Desafio 3-3

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,26 +1,11 @@
-const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll('.card')
-const modal = document.querySelector('.modal')
+
 for (let card of cards) {
     card.addEventListener('click', function() {
         const courseId = card.getAttribute('id')
-        modalOverlay.classList.add('active')
-        modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${courseId}`
+        window.location.href = `/course/${courseId}`
     })
 }
 
-document.querySelector('.close-modal').addEventListener('click', (e) => {
-    modalOverlay.classList.remove('active')
-    e.preventDefault()
-    modalOverlay.querySelector('iframe').src('')
-    
-})
 
-document.querySelector('.max-modal').addEventListener('click', () => {
-    if ( modal.classList.contains('maximize') ) {
-        modal.classList.remove('maximize')
-    } else {
-        modal.classList.add('maximize')
-    }
-})
 
